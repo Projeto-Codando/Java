67dc630b1d26ff1d2400ba5bbbc61e025a9737be
trajanomarcos <marcosvtrajano@outlook.com>
2024-08-18 22:20:11 -0300

    Codando - new: Criacao do metodo findByDescricao, onde o nome do avatar eh buscado.

--- api-codando/src/main/java/school/sptech/apicodando/api/domain/avatar/repository/AvatarRepository.java
+++ api-codando/src/main/java/school/sptech/apicodando/api/domain/avatar/repository/AvatarRepository.java
@@ -3,5 +3,8 @@ package school.sptech.apicodando.api.domain.avatar.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.sptech.apicodando.api.domain.avatar.Avatar;
 
+import java.util.Optional;
+
 public interface AvatarRepository extends JpaRepository<Avatar, Integer> {
+        Optional<Avatar> findByDescricao(String descricao);
 }
