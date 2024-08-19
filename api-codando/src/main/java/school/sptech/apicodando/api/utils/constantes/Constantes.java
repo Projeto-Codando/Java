fcdd318fa46c6cf3676464076ed5fa4a4e213463
trajanomarcos <marcosvtrajano@outlook.com>
2024-08-18 22:17:04 -0300

    Codando - new: Criacao de uma classe de constantes para que possamos utilizar tudo de apenas um local.

---
+++ api-codando/src/main/java/school/sptech/apicodando/api/utils/constantes/Constantes.java
@@ -0,0 +1,7 @@
+package school.sptech.apicodando.api.utils.constantes;
+
+public class Constantes {
+    public static final Integer MOEDAS_INICIAIS = 300;
+    public static final Integer ID_AVATAR_INICIAL = 1;
+
+}
