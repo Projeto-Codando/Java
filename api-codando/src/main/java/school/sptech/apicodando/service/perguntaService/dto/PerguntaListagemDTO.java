799060e46cc1b007005c9a5f6efb61b76beacd72
Matheus Ismael <matheus.lourenco@sptech.school>
2024-10-03 20:54:01 -0300

    Codando - feat: Implementação das Porcentagens de acertos e erros

--- api-codando/src/main/java/school/sptech/apicodando/service/perguntaService/dto/PerguntaListagemDTO.java
+++ api-codando/src/main/java/school/sptech/apicodando/service/perguntaService/dto/PerguntaListagemDTO.java
@@ -15,4 +15,25 @@ public class PerguntaListagemDTO {
     private Integer contador;
     private List<RespostaListagemDTO> respostas;
 
+    public String getPorcentagemRespostasIncorretas() {
+        return 100 - porcentagemRespostasCorretas() + "%";
+    }
+
+    public String getPorcentagemRespostasCorretas() {
+        return porcentagemRespostasCorretas() + "%";
+    }
+
+    public int porcentagemRespostasCorretas() {
+        int totalRespostas = 0;
+        int totalRespostasCorretas = 0;
+        for (RespostaListagemDTO resposta : respostas) {
+            totalRespostas++;
+            if (resposta.getCorreta()) {
+                totalRespostasCorretas++;
+            }
+        }
+        return totalRespostas == 0 ? 0 : (totalRespostasCorretas * 100 / totalRespostas);
+    }
+
+
 }
