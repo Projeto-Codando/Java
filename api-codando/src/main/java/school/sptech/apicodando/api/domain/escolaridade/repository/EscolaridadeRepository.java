dff9c644ffb7c31c7a8c9928506386ecd5313a19
trajanomarco <marcosvtrajano@outlook.com>
2024-10-25 23:21:22 -0300

    CODANDO: new: Implementacao de uma notacao que define os valores de Escolaridade.java, no banco de dados.

--- api-codando/src/main/java/school/sptech/apicodando/api/domain/escolaridade/repository/EscolaridadeRepository.java
+++ api-codando/src/main/java/school/sptech/apicodando/api/domain/escolaridade/repository/EscolaridadeRepository.java
@@ -1,7 +1,10 @@
 package school.sptech.apicodando.api.domain.escolaridade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school.sptech.apicodando.api.domain.escolaridade.Escolaridade;
 
+@Repository
 public interface EscolaridadeRepository extends JpaRepository<Escolaridade, Integer> {
+    long count();
 }
