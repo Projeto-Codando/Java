7501dd006f65d306179f2bac7af643cdecf2d53c
ismaelOG1 <matheus.lourenco@sptech.school>
2024-11-08 01:20:20 -0300

    Update SecurityConfiguracao.java

--- api-codando/src/main/java/school/sptech/apicodando/configuration/security/aluno/SecurityConfiguracao.java
+++ api-codando/src/main/java/school/sptech/apicodando/configuration/security/aluno/SecurityConfiguracao.java
@@ -58,6 +58,8 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/error/**"),
             new AntPathRequestMatcher("/educadores/**", HttpMethod.POST.name()),
             new AntPathRequestMatcher("/alunos/**", HttpMethod.POST.name()),
+            new AntPathRequestMatcher("/api/educadores/**", HttpMethod.POST.name()),
+            new AntPathRequestMatcher("/api/alunos/**", HttpMethod.POST.name()),
             new AntPathRequestMatcher("/csvFile/**"),
             new AntPathRequestMatcher("/turmas/gerarCSV/**")
 
